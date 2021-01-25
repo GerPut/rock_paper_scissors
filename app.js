e8f82aa64cc1f1584684db8ca93889deca7c0622
Gerhard <gerhard.putter@yahoo.com>
2021-01-25 11:21:50 +0200

    Html and JSconnected, button selections for options

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+
+/*
+  function computerPlay (){
+
+  }
+   let playerSelection;
+  let computerSelection;
+
+  "You Lose! Paper beats Rock"  */
+
+  const selectionButtons = document.querySelectorAll('[data-selection]')
+
+  selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        makeSelection(selectionName)
+    })
+  })
+
+  function makeSelection(selection) {
+    console.log(selection)
+  }
+
+
+
+
+
+  /*
+  function playRound(playerSelection, computerSelection) {
+    // your code here!
   }
   
-  console.log(getRandomInt(3));
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
+
+  function game(){
+
+  } */
